01b8662d88fc1d27d8315027882a5b818955d241
Nate Finch <n8finch@users.noreply.github.com>
2018-02-28 14:45:26 -0600

    get browsersync working 🔥🤓

--- gulpfile.js
+++ gulpfile.js
@@ -15,48 +15,9 @@ var download = require('gulp-download-stream');
 var browserSync = require('browser-sync').create();
 //=================================================//
 
-// Config
-var assetsDir = '/assets/'
-var config = {
-	bowerDir: '/bower_components',
-    jsDir: assetsDir + 'js',
-	sassDir: assetsDir + 'scss',
-	themeDir: '/',
-}
-//=================================================//
-
-// browser-sync watched files
-// automatically reloads the page when files changed
-var browserSyncWatchFiles = [
-  'style.css',
-  './assets/app.js',
-  './*.php'
-];
-
-// browser-sync options
-// see: https://www.browsersync.io/docs/options/
-var browserSyncOptions = {
-  proxy: "igb.test",
-  notify: true
-};
-
-
-//=================================================//
-
-// Attach bower_components here
-vendorJS = [
-  'bower_components/jquery/dist/jquery.min.js',
-  'bower_components/tether/dist/js/tether.min.js',
-  'bower_components/bootstrap/dist/js/bootstrap.min.js'
-]
-vendorCSS = [
-  'bower_components/bootstrap/dist/css/bootstrap.min.css'
-]
-//======================================================================//
-
 gulp.task('browser-sync', function() {
     browserSync.init({
-        proxy: "igd.test"
+        proxy: "mbio.test"
     });
 });
 
@@ -82,20 +43,6 @@ gulp.task('scripts', function() {
   .pipe(notify({ message: 'Scripts task complete' }));
 });
 
-// Compresses all of the bower JS assets included above
-gulp.task('vendor_js', function() {
-  gulp.src(vendorJS)
-  .pipe(concat('vendor.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('assets'))
-});
-
-// Compresses all of the bower JS assets included above
-gulp.task('vendor_css', function() {
-  gulp.src(vendorCSS)
-  .pipe(concat('vendor.min.css'))
-  .pipe(gulp.dest('assets'))
-});
 
 gulp.task('watch', function() {
 
@@ -113,4 +60,4 @@ gulp.task('hint', function() {
   .pipe(jshint.reporter('jshint-stylish'))
 });
 
-gulp.task('default', ['styles', 'scripts', 'watch']);
+gulp.task('default', ['styles', 'scripts', 'browser-sync', 'watch']);
